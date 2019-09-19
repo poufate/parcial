72a6838293b71cbf96506141b699ce23428ab73c
poufate <47480590+poufate@users.noreply.github.com>
2019-09-19 14:12:46 -0500

    ya casi
    falta la redireccion del login, arreglar la tabla y el agregado de cartas :)

--- js/js.js
+++ js/js.js
@@ -2,6 +2,8 @@ var jsonorigen = "js/cartas.json";
 const peticion = new XMLHttpRequest();
 var destinocontenido = document.querySelector("#Columnas");
 var datos;
+var campo=document.querySelector(".c");
+
 inicio();
 
 
@@ -10,6 +12,7 @@ inicio();
 
 function inicio() {
   cargar();
+
 }
 
 function cargar() {
@@ -19,35 +22,73 @@ function cargar() {
 
 		if(this.readyState == 4 ) {
 
-  let datos1 = JSON.parse(this.responseText);
-console.log(datos1);
-  localStorage.setItem("cartas", JSON.stringify(datos));
+
+
   peticion.onload = function() {
+    console.log("onload");
+
     datos = JSON.parse(this.responseText);
+    if (localStorage.getItem("cartas") === null) {
+      localStorage.setItem("cartas", JSON.stringify(datos));
+      datos=localStorage.getItem("cartas");
+     datos=JSON.parse(datos);
+      console.log("yes");
+    }else {
+      console.log("nel");
+      datos=localStorage.getItem("cartas");
+     datos=JSON.parse(datos);
+    }
 
     llenadoContenido(datos);
 
   }
 }}}
 
+$(".c").click(function () {
+
+  var can=$(this).attr("id");
+
+  sumar(can);
+});
+
 function sumar(numero) {
-console.log(numero);
+var data=localStorage.getItem("cartas");
+data=JSON.parse(data);
+var tempa=(parseInt(data[numero].cantidad)+1);
+data[numero].cantidad=""+tempa;
+localStorage.setItem("cartas",JSON.stringify(data));
+console.log("antes de pintar sumando"+data);
+removeAllChilds(destinocontenido);
+llenadoContenido(data);
 }
 
 function llenadoContenido(objetoJson) {
+  console.log(objetoJson);
 
-  for (i = 0; i < datos.length; i++) {
+  for (i = 0; i < objetoJson.length; i++) {
+
     var tr = document.createElement("tr");
     var id = document.createElement("td");
-    id.textContent = datos[i].numero;
+    id.textContent = objetoJson[i].numero;
     tr.appendChild(id);
     var carta = document.createElement("td");
-    carta.textContent = datos[i].carta;
+    carta.textContent = objetoJson[i].carta;
     tr.appendChild(carta);
     var cantidad = document.createElement("td");
-    cantidad.textContent = datos[i].cantidad;
+    cantidad.textContent = objetoJson[i].cantidad;
     tr.appendChild(cantidad);
     destinocontenido.appendChild(tr);
 
   }
+}
+function removeAllChilds(a)
+ {
+   if(a !== null){
+          while (a.hasChildNodes()){
+              a.removeChild(a.lastChild);
+          }
+ }
+ else {
+   console.log("no hay na");
+ }
 }
